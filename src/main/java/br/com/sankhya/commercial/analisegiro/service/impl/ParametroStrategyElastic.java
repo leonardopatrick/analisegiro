8ee1f9ad27997b9575f1e45a5c9bf4a86212eea8
Leonardo Patrick <leonardop@itout.com.br>
2021-08-17 13:38:11 -0300

    Ajuste ElasticSearch

--- src/main/java/br/com/sankhya/commercial/analisegiro/service/impl/ParametroStrategyElastic.java
+++ src/main/java/br/com/sankhya/commercial/analisegiro/service/impl/ParametroStrategyElastic.java
@@ -7,7 +7,6 @@ import br.com.sankhya.commercial.analisegiro.core.ParametroStrategy;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
